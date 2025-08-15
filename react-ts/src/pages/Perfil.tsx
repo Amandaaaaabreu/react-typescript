7c202f24a9ac08ea916f24101e3985ae0bf1a31b
Amandaaaaabreu <amandaabreu045@gmail.com>
2025-08-15 16:29:59 -0300

    estilizando a pagina de perfil

--- react-ts/src/pages/Perfil.tsx
+++ react-ts/src/pages/Perfil.tsx
@@ -1,7 +1,29 @@
 
 const Perfil = () => {
       return (
-      <h1>Perfil</h1>
+      <table className="table table-striped">
+            <thead>
+        <tr>
+          <th scope="col">Tech</th>
+          <th scope="col">Tipo</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>React</td>
+          <td>Frontend</td>
+        </tr>
+        <tr>
+          <td>Angular</td>
+          <td>Frontend</td>
+        </tr>
+        <tr>
+          <td>Node</td>
+          <td>Backend</td>
+        </tr>
+        
+      </tbody>
+      </table>
   );
 }
 
